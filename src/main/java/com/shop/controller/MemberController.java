a1e910841322e363e176d7b21b9db298461cfeeb
hyojpark <gkwk011@gmail.com>
2021-12-16 15:05:39 +0900

    validation 추가

--- src/main/java/com/shop/controller/MemberController.java
+++ src/main/java/com/shop/controller/MemberController.java
@@ -7,9 +7,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @RequestMapping("/members")
 @Controller
@@ -20,15 +23,25 @@ public class MemberController {
     private final PasswordEncoder passwordEncoder;
 
     @GetMapping(value = "/new")
-    public String memberForm(Model model) {
+    public String newMember(Model model) {
         model.addAttribute("memberFormDto", new MemberFormDto());
         return "member/memberForm";
     }
 
     @PostMapping(value = "/new")
-    public String memberForm(MemberFormDto memberFormDto) {
-        Member member = Member.createMember(memberFormDto, passwordEncoder);
-        memberService.saveMember(member);
+    public String newMember(@Valid MemberFormDto memberFormDto,
+                            BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            return "member/memberForm";
+        }
+
+        try {
+            Member member = Member.createMember(memberFormDto, passwordEncoder);
+            memberService.saveMember(member);
+        } catch (Exception e) {
+            model.addAttribute("errorMessage", e.getMessage());
+            return "member/memberForm";
+        }
 
         return "redirect:/";
     }
