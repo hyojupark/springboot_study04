7965b8863a1a7533877b1360cfe071e6293029b1
hyojpark <gkwk011@gmail.com>
2021-12-17 10:01:10 +0900

    항목별 권한 설정

--- src/main/java/com/shop/config/SecurityConfig.java
+++ src/main/java/com/shop/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -22,14 +23,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-//            .authorizeRequests()
-//                .antMatchers("/h2-console/**").permitAll()
-//                .antMatchers("/members/login/**").permitAll()
-//        .and()
-//            .headers().frameOptions().sameOrigin()
-//        .and()
-//            .csrf().requireCsrfProtectionMatcher(new AntPathRequestMatcher("!/h2-console/**"))
-//        .and()
+            .authorizeRequests()
+                .antMatchers("/h2-console/**").permitAll()
+                .antMatchers("/members/login/**").permitAll()
+        .and()
+            .headers().frameOptions().sameOrigin()
+        .and()
+            .csrf().requireCsrfProtectionMatcher(new AntPathRequestMatcher("!/h2-console/**"))
+        .and()
             .formLogin()
                 .loginPage("/members/login")
                 .defaultSuccessUrl("/")
@@ -41,6 +42,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .logoutSuccessUrl("/")
         ;
 
+        http.authorizeRequests()
+                .mvcMatchers("/", "/members/**", "/item/**", "/items/**").permitAll()
+                .mvcMatchers("/admin/**").hasRole("ADMIN")
+                .anyRequest().authenticated()
+        ;
+
+        http.exceptionHandling()
+                .authenticationEntryPoint(new CustomAuthenticationEntryPoint())
+        ;
     }
 
     @Bean
@@ -52,5 +62,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(memberService)
                 .passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/css/**", "/js/**", "/img/**");
     }
 }
