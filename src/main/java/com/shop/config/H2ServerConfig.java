ca926196ab700b40271625e11f5a71d6cfc133e2
hyojpark <gkwk011@gmail.com>
2021-12-20 10:47:56 +0900

    property 환경 별 h2 설정 변경 및 Database 연동을 위한 설정 추가

---
+++ src/main/java/com/shop/config/H2ServerConfig.java
@@ -0,0 +1,19 @@
+package com.shop.config;
+
+import org.h2.tools.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.sql.SQLException;
+
+@Configuration
+public class H2ServerConfig {
+
+    /**
+     * default port: 9092
+     */
+    @Bean
+    public Server h2TcpServer() throws SQLException {
+        return Server.createTcpServer("-tcp", "-tcpAllowOthers").start();
+    }
+}
