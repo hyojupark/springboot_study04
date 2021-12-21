e574399de82d154049bfe8d5fd30a4102d85164f
hyojpark <gkwk011@gmail.com>
2021-12-21 16:56:53 +0900

    상품 수정 관련 기능 추가

--- src/main/java/com/shop/entity/Item.java
+++ src/main/java/com/shop/entity/Item.java
@@ -1,6 +1,7 @@
 package com.shop.entity;
 
 import com.shop.constant.ItemSellStatus;
+import com.shop.dto.ItemFormDto;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -32,6 +33,12 @@ public class Item extends BaseEntity {
 
     @Enumerated(EnumType.ORDINAL)
     private ItemSellStatus itemSellStatus;  // 상품 판매 상태
-//    private LocalDateTime regTime;  // 등록 시간
-//    private LocalDateTime updateTime;  // 수정 시간
+
+    public void updateItem(ItemFormDto itemFormDto) {
+        this.itemNm = itemFormDto.getItemNm();
+        this.price = itemFormDto.getPrice();
+        this.stockNumber = itemFormDto.getStockNumber();
+        this.itemDetail = itemFormDto.getItemDetail();
+        this.itemSellStatus = itemFormDto.getItemSellStatus();
+    }
 }
--- src/main/java/com/shop/service/ItemImgService.java
+++ src/main/java/com/shop/service/ItemImgService.java
@@ -1,6 +1,5 @@
 package com.shop.service;
 
-import com.shop.controller.FileService;
 import com.shop.entity.ItemImg;
 import com.shop.repository.ItemImgRepository;
 import lombok.RequiredArgsConstructor;
@@ -10,14 +9,15 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 import org.thymeleaf.util.StringUtils;
 
-import java.io.IOException;
+import javax.persistence.EntityNotFoundException;
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
 public class ItemImgService {
 
-    @Value("${itemImgLocation}")
+    @Value("${itemImgLocation}/")
     private String itemImgLocation;
 
     private final ItemImgRepository itemImgRepository;
@@ -31,10 +31,25 @@ public class ItemImgService {
 
         if (!StringUtils.isEmpty(oriImgName)) {
             imgName = fileService.uploadFile(itemImgLocation, oriImgName, itemImgFile.getBytes());
-            imgUrl = "/images/item/" + imgName;
+            imgUrl = itemImgLocation + imgName;
         }
 
         itemImg.updateItemImg(oriImgName, imgName, imgUrl);
         itemImgRepository.save(itemImg);
+    }
+
+    public void updateItemImg(Long itemImgId, MultipartFile itemImgFile) throws Exception {
+        if (!itemImgFile.isEmpty()) {
+            ItemImg savedItemImg = itemImgRepository.findById(itemImgId).orElseThrow(EntityNotFoundException::new);
+
+            if (!StringUtils.isEmpty(savedItemImg.getImgName())) {
+                fileService.deleteFile(savedItemImg.getImgUrl());
+            }
+
+            String oriImgName = Objects.requireNonNull(itemImgFile.getOriginalFilename());
+            String imgName = fileService.uploadFile(itemImgLocation, oriImgName, itemImgFile.getBytes());
+            String imgUrl = itemImgLocation + imgName;
+            savedItemImg.updateItemImg(oriImgName, imgName, imgUrl);
+        }
     }
 }
